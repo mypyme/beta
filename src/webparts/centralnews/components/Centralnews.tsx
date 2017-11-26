1b6ed9736f2afcb8d15a3e757fdbc367344583ac
Diego Jimenez <djimenez@velocitypartners.net>
2017-11-26 16:20:13 -0500

    adding styles to central news webpart

--- src/webparts/centralnews/components/Centralnews.tsx
+++ src/webparts/centralnews/components/Centralnews.tsx
@@ -6,13 +6,18 @@ import { escape } from '@microsoft/sp-lodash-subset';
 export default class CentralNews extends React.Component<ICentralNewsProps, {}> {
   public render(): React.ReactElement<ICentralNewsProps> {
     return (
-      <div className='news-container'>
+      <div className={styles.centralnews}>
+        <h3>Cuídate / Cuidame</h3>
         {this.props.news.value.map(function (item, index) {
           var rawMarkup= item.Noticia;
-          return <div>
-            <div><a href={item.Link.Url}>{item.Title}</a></div>
-            <img src={item.Imagen.Url}></img>
-            <div dangerouslySetInnerHTML={{__html: rawMarkup}}></div>
+          return <div className={styles.new}>
+            <div className={styles.image}>
+              <img src={item.Imagen.Url}></img>
+            </div>
+            <div className={styles.detail}>
+              <a href={item.Link.Url}>{item.Title}</a>
+              <span dangerouslySetInnerHTML={{__html: rawMarkup}}></span>
+            </div>
           </div>
         })}
       </div>
