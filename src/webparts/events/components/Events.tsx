c6ad1816f2204def2869c6ca8f6ea6274ff7491e
Diego Jimenez <djimenez@velocitypartners.net>
2017-11-26 22:52:04 -0500

    Adding styles to the events webpart

--- src/webparts/events/components/Events.tsx
+++ src/webparts/events/components/Events.tsx
@@ -6,15 +6,19 @@ import { escape } from '@microsoft/sp-lodash-subset';
 export default class Events extends React.Component<IEventsProps, {}> {
   public render(): React.ReactElement<IEventsProps> {
     return (
-      <div className='events-container'>
-        <span>Today's Events</span>
+      <div className={styles.events}>
+        <h3>Eventos de Hoy</h3>
         {this.props.CurrentEvents.value.map(function (item, index) {
           let initial = new Date(item.EventDate).getHours()+':'+new Date(item.EventDate).getMinutes();
           let endDate = new Date(item.EndDate).getHours()+':'+new Date(item.EndDate).getMinutes();
           
-          return <div>
-            <div>{item.Title}</div>
-            <div>{initial}-{endDate}</div>
+          return <div className={styles.event}>
+            <div className={styles.image}/>
+            <div className={styles.detail}>
+              <div className={styles.title}>{item.Title}</div>
+              <div><b>Lugar:</b> {item.Location}</div>
+              <div><b>Hora:</b> {initial}-{endDate}</div>
+            </div>
           </div>
         })}
       </div>
